da0998d176dbf982f9b7e165971d89927b3a73f2
Nathan Donnellan <ndonnellan@gmail.com>
2013-12-22 10:29:37 -0600

    Removed unneeded overrides

--- _overrides.rb
+++ _overrides.rb
@@ -1,22 +1,3 @@
 class Object
-  def to_pct_str(format)
-    unless self.class == Fixnum || self.class == Float
-      raise "to_pct_str is not supported for #{self.class}"
-    end
 
-    n_str = sprintf format, self
-    if self >= 0
-      "+" + n_str
-    else
-      n_str
-    end
-  end
-
-  def usd
-    Usd.new(self)
-  end
-
-  def ir
-    Rate.new(self)
-  end
 end
